5f7c37a5d7b08a697302e6067c5fe70008fd9d5e
jaybaldwa30 <jaybaldwa123@gmail.com>
2024-03-20 22:10:44 +0530

    updated with single state functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,71 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard/GameBoard";
 import Player from "./components/Player/Player";
 import Log from "./components/Log/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations";
+import GameOver from "./components/GameOver/GameOver";
 
+const initialBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+function deriveActivePlayer(gameTurns) {
+  let currentPlayer = "X";
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  return currentPlayer;
+}
 //now to share the state between two componenets we have the functionalirty to lift the state up, where we pass the state to diferent components as props
 function App() {
-  const [activePlayer, setactivePlayer] = useState("X");
-  function handleSelectSquare() {
-    setactivePlayer((currentActivePlayer) =>
-      currentActivePlayer === "X" ? "O" : "X"
-    );
+  const [gameTurns, setGameTurns] = useState([]);
+  // const [hasWinner, setHasWinner] = useState(false);
+
+  const activePlayer = deriveActivePlayer(gameTurns);
+  let gameBoard = [...initialBoard.map((array) => [...array])];
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+  let winner;
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column];
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column];
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column];
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol;
+    }
+  }
+  const hasDraw = gameTurns.length === 9 && !winner;
+
+  // const [activePlayer, setactivePlayer] = useState("X");
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    // setactivePlayer((currentActivePlayer) =>
+    //   currentActivePlayer === "X" ? "O" : "X"
+    // );
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
+      return updatedTurns;
+    });
+  }
+
+  function handleRestart() {
+    setGameTurns([]);
   }
   return (
     <>
@@ -27,13 +84,13 @@ function App() {
               isActive={activePlayer === "O"}
             />
           </ol>
-          <GameBoard
-            onSelectSquare={handleSelectSquare}
-            activePlayerSymbol={activePlayer}
-          />
+          {(winner || hasDraw) && (
+            <GameOver winner={winner} onRestart={handleRestart} />
+          )}
+          <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
         </div>
         <div>
-          <Log />
+          <Log turns={gameTurns} />
         </div>
       </main>
     </>
